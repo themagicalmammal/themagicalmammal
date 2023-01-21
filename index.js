58664bdac5ed40790f174e1d93e51ce537ecc8f2
Dipan Nanda <43346471+themagicalmammal@users.noreply.github.com>
2023-01-21 15:12:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -40,17 +40,6 @@ async function setWeatherInformation() {
     });
 }
 
-async function setInstagramPosts() {
-  const instagramImages =
-    await puppeteerService.getLatestInstagramPostsFromAccount(
-      "london4visit",
-      3
-    );
-  DATA.img1 = instagramImages[0];
-  DATA.img2 = instagramImages[1];
-  DATA.img3 = instagramImages[2];
-}
-
 async function generateReadMe() {
   await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
@@ -64,11 +53,6 @@ async function action() {
    * Fetch Weather
    */
   await setWeatherInformation();
-
-  /**
-   * Get pictures
-   */
-  await setInstagramPosts();
 
   /**
    * Generate README
