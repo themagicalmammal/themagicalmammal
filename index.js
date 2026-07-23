0774da4e79db8ae5eae25dc5c53204352b17970a
Dipan Nanda <dipangithub@gmail.com>
2026-07-23 08:22:48 +0100

    Fix vulnerabilities and update all dependencies

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const Mustache = require("mustache");
-const fetch = require("node-fetch");
 const fs = require("fs");
 
 const MUSTACHE_MAIN_DIR = "./main.mustache";
@@ -18,33 +17,29 @@ const DATA = {
 };
 
 async function setWeatherInformation() {
-  await fetch(
+  const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`,
-  )
-    .then((r) => r.json())
-    .then((r) => {
-      DATA.city_temperature = Math.round(r.main.temp);
-      DATA.city_weather = r.weather[0].description;
-      DATA.city_weather_icon = r.weather[0].icon;
-      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString("en-GB", {
-        hour: "2-digit",
-        minute: "2-digit",
-        timeZone: "Europe/London",
-      });
-      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString("en-GB", {
-        hour: "2-digit",
-        minute: "2-digit",
-        timeZone: "Europe/London",
-      });
-    });
+  );
+  const r = await response.json();
+  DATA.city_temperature = Math.round(r.main.temp);
+  DATA.city_weather = r.weather[0].description;
+  DATA.city_weather_icon = r.weather[0].icon;
+  DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Europe/London",
+  });
+  DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Europe/London",
+  });
 }
 
 async function generateReadMe() {
-  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync("README.md", output);
-  });
+  const data = await fs.promises.readFile(MUSTACHE_MAIN_DIR);
+  const output = Mustache.render(data.toString(), DATA);
+  fs.writeFileSync("README.md", output);
 }
 
 async function action() {
